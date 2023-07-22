2c5746b7a0c039ba0b644081c7d488f8f9343ab9
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-22 12:11:57 -0300

    last commit

--- src/script.js
+++ src/script.js
@@ -1,31 +1,45 @@
 const input = document.querySelector('#input-task');
 const addTaskButton = document.querySelector('#add-task');
-const ul = document.querySelector('.list-container');
-const container = document.querySelector('.container');
+const listContainer = document.querySelector('.list-container');
 
 const addTask = () => {
     const textTask = input.value.trim();
     
     if (textTask.length === 0) {
         return;
-    }
-
+    } 
     const li = document.createElement('li');
     li.innerText = textTask;
-    ul.appendChild(li);
-
-    li.addEventListener('click', () => {
-        li.classList.toggle('checked');
-    })
-
+    listContainer.appendChild(li);
+    const span = document.createElement('span');
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    
     input.value = "";
+    saveData();
 }
 
 addTaskButton.addEventListener('click', addTask);    
-input.addEventListener('keyup', (e) => {
-    if (e.code === "Enter") {
-        addTask();
+
+input.addEventListener('keyup', (e) =>  e.code === "Enter" ? addTask() : null);
+
+listContainer.addEventListener('click', (e) => {
+    const selected = e.target;
+    if (selected.tagName === "LI") {
+        selected.classList.toggle('checked');
+        saveData();
+    } else if (selected.tagName === "SPAN") {
+        selected.parentElement.remove();
+        saveData();
     }
 })
 
-console.log(container.computedStyleMap)
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
